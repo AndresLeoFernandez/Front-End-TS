6f811d6c3eff0c464eb37ca51c08adcbfbd0a476
AndresLeoFernandez <104107412+AndresLeoFernandez@users.noreply.github.com>
2022-08-06 17:06:08 -0300

    agrego ordenamiento burbuja

--- src/index.ts
+++ src/index.ts
@@ -112,6 +112,7 @@ let cantidad: number = 10; // Cantidad total de elementos del arreglo.
 let clientes: string[] = new Array(cantidad); // Arreglo para alojar nombres
 let facturacion: number[] = new Array(cantidad); // Arreglo para alojar valor numerico
 let btnAgregarClientes = document.getElementById("btn-cargar-clientes");
+let btnVerConsola = document.getElementById("btn-ver-consola");
 let listaClientes = document.getElementById("lista-clientes");
 
 const cargarClientes = () => {
@@ -139,13 +140,109 @@ const cargarClientes = () => {
       clientes[posicion]
     }</td><td>${facturacion[posicion]}</td><tr>`;
   }
+  btnVerConsola?.classList.remove("ocultar");
+  btnVerConsola?.classList.add("mostrar");
 };
-/*
 const mostrasCincoMejoresClientes = () => {
   for (let posicion: number = 0; posicion < 4; posicion++) {
     console.log(
-      `(${posicion}) ${clientes[posicion]}, ${facturacion[posicion]}`
+      `(${posicion + 1}) ${clientes[posicion]}, ${facturacion[posicion]}`
     );
   }
-};*/
+};
+
 btnAgregarClientes?.addEventListener("click", cargarClientes);
+btnVerConsola?.addEventListener("click", mostrasCincoMejoresClientes);
+
+// Generales
+function Azar(tope: number): number {
+  return Math.floor(Math.random() * tope);
+}
+
+/*
+ Este método permite cargar un arreglo “ de dimensión “ y
+ llenarlo de valores generados al azar entre 0 y “ numAzar”
+*/
+function cargar(arreglo: number[], cantidad: number, numAzar: number) {
+  for (let i: number = 0; i < cantidad; i++) {
+    arreglo[i] = Azar(numAzar);
+  }
+}
+
+/*
+Este método permite mostrar un arreglo de dimensión cantidad en una única línea,
+ separando los valores con un espacio.
+*/
+function escribirEnUnaLinea(arreglo: number[], cantidad: number) {
+  let vector: string = "";
+  for (let i: number = 0; i < cantidad; i++) {
+    vector += `${arreglo[i]} - `;
+  }
+  console.log(vector);
+}
+
+/*
+Este método permite intercambiar los valores en las
+posiciones “ y “ de un arreglo “ utilizando una variable auxiliar
+*/
+function intercambiar(arreglo: number[], i: number, j: number) {
+  let aux: number;
+  aux = arreglo[i];
+  arreglo[i] = arreglo[j];
+  arreglo[j] = aux;
+}
+/*
+Este método permite comparar los valores en las posiciones “i”
+y “j” del arreglo “arreglo”
+• Devuelve 0 si son iguales,
+• 1 si lo que hay en “i” es mayor a lo que hay en “j”
+• -1 si lo que hay en “i” es menor a lo que hay en “j”
+*/
+function comparar(arreglo: number[], i: number, j: number): number {
+  let comparacion: number;
+  if (arreglo[i] === arreglo[j]) {
+    comparacion = 0;
+  } else if (arreglo[i] < arreglo[j]) {
+    comparacion = -1;
+  } else {
+    comparacion = 1;
+  }
+  return comparacion;
+}
+/*
+Metodo Burbuja
+*/
+function ordenamientoBurbuja(arreglo: number[], cantidad: number) {
+  let i: number, j: number;
+  for (i = 2; i < cantidad; i++) {
+    for (j = 0; j < cantidad - 1; j++) {
+      if (comparar(arreglo, j, j + 1) === 1) {
+        intercambiar(arreglo, j, j + 1);
+      }
+    }
+  }
+}
+let btnCargarArreglo = document.getElementById("btn-cargar-arreglo");
+let visorEjercicio8 = document.getElementById("visor-8");
+btnCargarArreglo?.addEventListener("click", () => {
+  visorEjercicio8?.innerHTML = "";
+  console.log("Creo el limite con valor de 10.");
+  visorEjercicio8?.innerHTML += `Creo el limite con valor de 10...<br/>`;
+  let lim: number = 10;
+  console.log("Creo A con espacio para 10 elementos.");
+  visorEjercicio8?.innerHTML += `Creo arreglo A con espacio para 10 elementos...<br/>`;
+  let a: number[] = new Array(lim);
+  console.log("Asigno al arreglo A 10 elementos al Azar.");
+  visorEjercicio8?.innerHTML += `Asigno al arreglo A 10 elementos al Azar...<br/>`;
+  cargar(a, lim, 100);
+  console.log("Muestro el A cargado al Azar.");
+  visorEjercicio8?.innerHTML += `Valores del Arreglo: ${a.toString()} <br/>`;
+  escribirEnUnaLinea(a, lim);
+  console.log("Aplico Ordenamiento de Burbuja.");
+  visorEjercicio8?.innerHTML += `Aplico Ordenamiento de Burbuja...<br/>`;
+  ordenamientoBurbuja(a, lim);
+  console.log("Muestro A ordenado.");
+  visorEjercicio8?.innerHTML += `Valores del Arreglo Ordenado: ${a.toString()} <br/>`;
+  escribirEnUnaLinea(a, lim);
+  visorEjercicio8?.innerHTML += `Fin de las tareas. <br/>`;
+});
